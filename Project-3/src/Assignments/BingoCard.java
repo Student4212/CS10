ede419f6ffd561a1e414d983d09498157092a118
16261507 <16261507@L815WD220012.edu.cbe.ab.ca>
2022-04-04 12:52:48 -0600

    12:52 pm

---
+++ Project-3/src/Assignments/BingoCard.java
@@ -0,0 +1,22 @@
+package Assignments;
+
+public class BingoCard {
+
+	public static void main(String[] args) {
+		System.out.println(""
+				+ "B     I     N     G     O"
+				+ "\r\n"
+				+ "11    28    36    58    70"
+				+ "\r\n"
+				+ "2     17    34    50    62"
+				+ "\r\n"
+				+ "6     23    Free  49    73"
+				+ "\r\n"
+				+ "14    27    39    54    65"
+				+ "\r\n"
+				+ "4     19    42    52    69"
+				+ "\r\n");
+
+	}
+
+}
