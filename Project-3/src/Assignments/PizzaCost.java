644d53a124b220c7834b19e2c924a1af77c9b9d0
16261507 <16261507@L815WD220012.edu.cbe.ab.ca>
2022-04-05 15:16:23 -0600

    3:15 pm

--- Project-3/src/Assignments/PizzaCost.java
+++ Project-3/src/Assignments/PizzaCost.java
@@ -1,12 +1,28 @@
 package Assignments;
+import java.util.*;
+
+
 
 public class PizzaCost 
 {
 
 	public static void main(String[] args) 
 	{
-		int size = _; 
-		System.out.println()
+		Scanner myObj = new Scanner(System.in); 
+	    System.out.println("Enter Pizza Diameter in Whole Inches");
+
+	    int size = myObj.nextInt(); 
+	    int size2 = Integer.parseInt(size);
+	    
+	    OR
+	    
+	    String size = myObj.nextInt(); 
+	    int size2 = Integer.parseInt(size);
+	   
+	    
+	    float extra = 1.75f;
+	    int a = size2*size2;
+	    System.out.println("It costs" + a+extra);  
 
 	}
 
