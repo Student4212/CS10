38bf0c65d29ad8dc26cfd9aa3c62fe30d328a7f7
16261507 <16261507@L815WD220012.edu.cbe.ab.ca>
2022-04-06 13:52:49 -0600

    1:52 pm

---
+++ Project-3/src/Assignments/BluePelicanJava.java
@@ -0,0 +1,31 @@
+package Assignments;
+
+public class BluePelicanJava 
+{
+	/*
+
+	Program: BingoCard.java          Date: April 6th 
+
+
+	Author: Naomi Lockerbie  
+	School: CHHS
+	Course: Computer Science 10
+	
+
+	*/
+	public static void main(String[] args) 
+	{
+		 int first = 79 + 3 * (4 + 82 - 68) - 7 +19;
+		 int second = (179 +21 +10) / 7 + 181; 
+		 int third = 10389 * 56 * 11 + 2246; 
+		 System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 =" [first] "\r\n"
+		 		+ "(179 + 21 + 10) / 7 + 181 = " second "\r\n"
+		 		+ "10389 * 56 * 11 + 2246 = " third);
+	}
+
+}
+/* Screen Dump
+
+
+
+ */
