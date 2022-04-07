0f7dd3be46fefb45c70ef922b5f3afe343b826e5
16261507 <16261507@L815WD220012.edu.cbe.ab.ca>
2022-04-07 14:58:37 -0600

    2:58 pm

--- Project-3/src/Assignments/Assign1.java
+++ Project-3/src/Assignments/Assign1.java
@@ -19,15 +19,24 @@ public class Assign1 {
 		    public static void main(String args[])
 		    {
 		       Scanner input = new Scanner(System.in);
-		       System.out.println("Heyyyyyy!");
-		       String fullName = input.next();
+		       System.out.println("Whats your name!");
+		       String fullName = input.nextLine();
+		       System.out.println(fullName);
+		       System.out.println("How old are you?");
+		       int age = input.nextInt(); 
+		       System.out.println(age);
 		    }
 
 
 
 		/* Screen Dump
 		 
-
+Whats your name!
+naomi
+naomi
+How old are you?
+2
+2
 
 		 
 		 */
--- Project-3/src/Assignments/BluePelicanJava.java
+++ Project-3/src/Assignments/BluePelicanJava.java
@@ -4,7 +4,7 @@ public class BluePelicanJava
 {
 	/*
 
-	Program: BingoCard.java          Date: April 6th 
+	Program: BluePelicanJava.java          Date: April 6th 
 
 
 	Author: Naomi Lockerbie  
@@ -18,14 +18,16 @@ public class BluePelicanJava
 		 int first = 79 + 3 * (4 + 82 - 68) - 7 +19;
 		 int second = (179 +21 +10) / 7 + 181; 
 		 int third = 10389 * 56 * 11 + 2246; 
-		 System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 =" [first] "\r\n"
-		 		+ "(179 + 21 + 10) / 7 + 181 = " second "\r\n"
-		 		+ "10389 * 56 * 11 + 2246 = " third);
+		 System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 =" + first + "\r\n"
+		 		+ "(179 + 21 + 10) / 7 + 181 = " + second + "\r\n"
+		 		+ "10389 * 56 * 11 + 2246 = " + third);
 	}
 
 }
 /* Screen Dump
 
-
+79 + 3 * (4 + 82 - 68) - 7 + 19 =145
+(179 + 21 + 10) / 7 + 181 = 211
+10389 * 56 * 11 + 2246 = 6401870
 
  */
