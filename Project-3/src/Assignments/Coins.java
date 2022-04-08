fd41947eb106bd44c23ca0ee3da5b032fa5e7c42
16261507 <16261507@L815WD220012.edu.cbe.ab.ca>
2022-04-08 11:40:03 -0600

    11:39 pm

---
+++ Project-3/src/Assignments/Coins.java
@@ -0,0 +1,45 @@
+package Assignments;
+import java.util.Scanner;
+public class Coins 
+{
+	/*
+
+	Program: Coins.java          Date: April 8th 
+
+
+	Author: Naomi Lockerbie  
+	School: CHHS
+	Course: Computer Science 10
+	
+
+	*/
+	public static void main(String[] args) 
+	{
+		Scanner myObj = new Scanner(System.in); 
+	    System.out.println("How much money do you have in coins written in cents");
+	    int change = myObj.nextInt(); 		
+	    System.out.println("The minimum change change you have is " + change);
+		int quarters = change / 25;
+		System.out.println("Quarters: " + quarters);
+		int dimes = (change % 25)/10;
+		System.out.println("Dimes: " + dimes);
+		int nickels = ((change % 25)%10)/5;
+		System.out.println("Nickels: " + nickels);
+		int pennies = (((change % 25)%10)%5);
+		System.out.println("Pennies: " + pennies);
+
+	}
+
+}
+
+/* Screen Dump
+
+How much money do you have in coins written in cents
+213
+The minimum change change you have is 213
+Quarters: 8
+Dimes: 1
+Nickels: 0
+Pennies: 3
+
+ */
---
+++ Project-3/src/Assignments/Practice.java
@@ -0,0 +1,15 @@
+package Assignments;
+import java.util.Scanner;
+public class Practice 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner userinput - now Scanner(System.in);
+		System.out.print("please enter your full name");
+		String name = userinput.nextLine();
+		System.out.print("your full name is"+ name);
+	
+	}
+
+}
