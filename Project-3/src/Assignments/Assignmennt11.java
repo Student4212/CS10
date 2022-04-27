013c5e735e50b927b44e958805a8da5368da5ab7
16261507 <16261507@L815WD220012.edu.cbe.ab.ca>
2022-04-27 13:41:52 -0600

    1:41 pm

--- Project-3/src/Assignments/Assignmennt11.java
+++ Project-3/src/Assignments/Assignmennt11.java
@@ -1,3 +1,14 @@
+/*
+
+	Program: Assignment11.java          Date: April 27th 
+
+
+	Author: Naomi Lockerbie  
+	School: CHHS
+	Course: Computer Science 10
+	
+
+	*/
 package Assignments;
 import java.util.Scanner;
 public class Assignmennt11 
@@ -12,34 +23,34 @@ public class Assignmennt11
 		int second = (all%100)/10; //extracts the second digit by finding the remainder you get when you divide by 100 and then dividing that by 10 
 		int third = ((all%100)%10); //extracts the third digit by finding the remainder you get when you divide by 100 and then the remainder you get by dividing that remainder by 10 
 
-		int max = 0; 
-		int min = 0; 
+		int max = 0; //defines max as 0 
+		int min = 0; //defines min as 0 
 		
-		if (second>first && third>first)
+		if (second>first && third>first)// says that second and third are larger than first to follow the instructions in the curly bracket
 		{
-			min=first; 
+			min=first; // define min as the value of first 
 		}
-		else if (first>second && third>second) 
+		else if (first>second && third>second) //says that first and third are larger than second to follow the instructions in the curly bracket
 		{
-			min = second; 
+			min = second; //Defines min as the value of second
 		}
-		else 
+		else //says that if  second and third are not larger than first and first and third are not larger than second to follow the instructions in the curly bracket
 		{
-			min = third; 
+			min = third; //defines min as the value of third 
 		}
-		if (second>first && second>third) 
+		if (second>first && second>third) //says that if first and third are smaller than second to follow the instructions in the curly bracket
 		{
-			max = second; 
+			max = second; //defines max as the value of second 
 		}
-		else if (third>first && third>second)
+		else if (third>first && third>second)//says that if first and second are smaller than third to follow the instructions in the curly bracket
 		{
-			max = third; 
+			max = third; //defines max as the value of third
 		}
-		else 
+		else //says that if first and second are not smaller than third and first and third are not smaller than second to follow the instructions in the curly bracket
 		{
-			max = first; 
+			max = first; //defines max as the value of first  
 		}
-		System.out.println("Max: "+max+"  Min: "+min);
+		System.out.println("Max: "+max+"  Min: "+min);//prints out the value of min and the value of max and explains what they are 
 	}
 
 }
---
+++ Project-3/src/Assignments/javaSwitch.java
@@ -0,0 +1,35 @@
+package Assignments;
+
+public class javaSwitch 
+{
+
+	public static void main(String[] args) 
+	{
+		int day = 2;
+		switch (day) 
+		{
+			case 1:
+				System.out.println("Saturday");
+				break;
+		  
+			case 2:
+				System.out.println("Sunday");
+				break;
+		}
+		int day2 = 4;
+		switch (day2) 
+		{
+			case 1:
+				System.out.println("Saturday");
+				break;
+			case 2:
+		    System.out.println("Sunday"); 
+		    break;
+		  
+		default:
+		    System.out.println("Weekend");
+		}
+
+	}
+
+}
