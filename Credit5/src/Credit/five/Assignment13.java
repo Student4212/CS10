6d6a6626e4abc19ac24de2e1d8b2d5a8c5345b16
16261507 <16261507@L815WD220012.edu.cbe.ab.ca>
2022-05-03 15:10:13 -0600

    3:10 pm

--- Credit5/src/Credit/five/Assignment13.java
+++ Credit5/src/Credit/five/Assignment13.java
@@ -19,33 +19,33 @@ public class Assignment13
 	public static void main(String[] args) 
 	{
 		Scanner input = new Scanner(System.in);//creates new scanner 
-		System.out.println("Enter the number of copies to be printed: "); 
-		int num = input.nextInt(); //Records answer
-		float single = 0f;
-		if (num>=0 && num<=99)
+		System.out.println("Enter the number of copies to be printed: "); //displays request that you enter the number of copies you need to print 
+		int num = input.nextInt(); //Records answer as variable num
+		float single = 0f;//creates variable (float) single and gives it a value of 0 
+		if (num>=0 && num<=99)//says if number of copies/num is greater than or equal to 0 and less than or equal to 99, follow the instructions inside the bracket 
 		{
-			single = 0.30f; 
+			single = 0.30f; //stores 0.30 as the value of single 
 		}
-		else if (num>=100 && num<=499)
+		else if (num>=100 && num<=499)//if the number of copie/num does not fit the previous conditions and it is greater than or equal to 100 and less than or equal to 499, follow the instructions inside the bracket 
 		{
-			single = 0.28f; 
+			single = 0.28f; //stores 0.28 as the value of single 
 		}
-		else if (num>=500 && num<=749)
+		else if (num>=500 && num<=749)//if the number of copies/num does not fit the previous conditions and it is greater than or equal to 500 and less than or equal to 749, follow the instructions inside the bracket 
 		{
-			single = 0.27f; 
+			single = 0.27f; //stores 0.27 as the value of single 
 		}
-		else if (num>=750 && num<=1000)
+		else if (num>=750 && num<=1000)//if the number of copies/num does not fit the previous conditions and it is greater than or equal to 750 and less than or equal to 1000, follow the instructions inside the bracket 
 		{
-			single = 0.26f; 
+			single = 0.26f; //stores 0.26 as the value of single 
 		}
-		else 
+		else //if the number of copies/num does not fit the previous conditions, follow the instructions inside the bracket 
 		{
-			single = 0.25f; 
+			single = 0.25f; //stores 0.25 as the value of single 
 		}
 		
-		float total = single*num; 
+		float total = single*num; //multiply number of copies/num by the value of single (the price of a single copy printed )
 		System.out.println("Price per copy is: $"+single+"\r\n"
-				+ "Total cost is: $"+total);
+				+ "Total cost is: $"+total);//display prices and totals
 			
 	}
 
