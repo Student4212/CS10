64f10fe8d9777f3922edb0486611970c8422fe20
16261507 <16261507@L815WD220012.edu.cbe.ab.ca>
2022-05-12 15:03:47 -0600

    3:03 pm

--- Credit5/src/Credit/five/Assignment17.java
+++ Credit5/src/Credit/five/Assignment17.java
@@ -1,11 +1,46 @@
 package Credit.five;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Assignment17 
 {
 
 	public static void main(String[] args) 
 	{
-		
+		Scanner input = new Scanner(System.in); 
+		DecimalFormat df = new DecimalFormat("0.00");
+		System.out.println("Please enter your GPA or enter -1 to quit the program: ");
+		double gpa = input.nextDouble(); 
+		while(gpa == -1)
+		{
+			gpa++;
+		}
+		if(gpa >= 3.80)
+		{
+			System.out.println("summa cum laude");
+			System.out.println("Enter another GPA or type -1 to quit?");
+			
+		}
+		else if(gpa >= 3.65)
+		{
+			System.out.println("magna cum laude");
+			System.out.println("Enter another GPA or type -1 to quit?");
+			
+		}
+		else if(gpa >= 3.50)
+		{
+			System.out.println("cum laude");
+			System.out.println("Enter another GPA or type -1 to quit?");
+			
+		}
+		else if (gpa <= 3.5 && gpa > 0)
+		{
+			System.out.println("Sorry, you did not qualify for an honors distinction\r\n"
+					+ "Would you like to continue or quit?");
+
+		}
+		System.out.println("Thank you for using our program, have a great day!");
 
 	}
 
