708805c4c9e7874dbef64c6e11afe816bf19c0a1
16261507 <16261507@L815WD220012.edu.cbe.ab.ca>
2022-05-13 11:04:51 -0600

    11:04

--- Credit5/src/Credit/five/RockPaperSicissors.java
+++ Credit5/src/Credit/five/RockPaperSicissors.java
@@ -1,3 +1,13 @@
+/*
+
+	Program: RockPaperSicissors.java          Date: May 13th 
+
+
+	Author: Naomi Lockerbie  
+	School: CHHS
+	Course: Computer Science 10
+
+*/
 package Credit.five;
 
 import java.util.Scanner;
@@ -9,47 +19,52 @@ public class RockPaperSicissors
 	{
 		Scanner input = new Scanner(System.in);//creates new scanner
 		int comp = 1 + (int)(Math.random()*3);
-		System.out.println("Choose rock paper or scissors: "); 
+		System.out.println("Enter rock paper or scissors: "); 
 		String player = input.nextLine(); 
-		String message = "0";
-		if (player=="rock" && comp==1)
+		String message = "Error, try entering again";		
+		if (player.equals("rock") && comp==1)
 		{
 			message = "Computer: rock \r\nIt's a Tie";
 		}
-		else if (player=="paper" && comp==2)
+		else if (player.equals("paper") && comp==2)
 		{
 			message = "Computer: paper \r\nIt's a Tie";
 		}
-		else if (player=="scissors" && comp==3)
+		else if (player.equals("scissors") && comp==3)
 		{
 			message = "Computer: scissors \r\nIt's a Tie";
 		}
-		else if (player=="rock" && comp==2)
+		else if (player.equals("rock") && comp==2)
 		{
 			message = "Computer: paper \r\nYou lose!";
 		}
-		else if (player=="paper" && comp==3)
+		else if (player.equals("paper") && comp==3)
 		{
 			message = "Computer: scissors \r\nYou lose!";
 		}
-		else if (player=="scissors" && comp==1)
+		else if (player.equals("scissors") && comp==1)
 		{
 			message = "Computer: rock \r\nYou lose!";
 		}
-		else if (player=="rock" && comp==3)
+		else if (player.equals("rock") && comp==3)
 		{
 			message = "Computer: scissors \r\nYou win!";
 		}
-		else if (player=="paper" && comp==1)
+		else if (player.equals("paper") && comp==1)
 		{
 			message = "Computer: rock \r\nYou win!";
 		}
-		else if (player=="scissors" && comp==2)
+		else if (player.equals("scissors") && comp==2)
 		{
 			message = "Computer: paper \r\nYou win!";
 			
 		}
-		System.out.println("You: "+player+" "+message);
+		System.out.println("You: "+player+"\r\n"+message);
 	}
-
 }
+/*Screen Dump
+Choose rock paper or scissors: 
+rock
+You: rock Computer: scissors 
+You win!
+*/
