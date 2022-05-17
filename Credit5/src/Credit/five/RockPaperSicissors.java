2e32daa9004b74fb3681187c9af77c243e49b8f5
16261507 <16261507@L815WD220012.edu.cbe.ab.ca>
2022-05-17 14:15:02 -0600

    2:14PM

--- Credit5/src/Credit/five/RockPaperSicissors.java
+++ Credit5/src/Credit/five/RockPaperSicissors.java
@@ -21,43 +21,43 @@ public class RockPaperSicissors
 		while (one.equalsIgnoreCase("play")) 
 		{
 		Scanner input = new Scanner(System.in);//creates new scanner
-		int comp = 1 + (int)(Math.random()*3);
-		System.out.println("\r\nEnter rock paper or scissors: "); 
-		String player = input.nextLine(); 
-		String message = "Error, try entering again";		
-		if (player.equalsIgnoreCase("rock") && comp==1)
+		int comp = 1 + (int)(Math.random()*3);//genterates a random number from 1-3 and stores that number as variable comp
+		System.out.println("\r\nEnter rock paper or scissors: "); //asks you to enter rock paper or scissors
+		String player = input.nextLine(); //records your answer as variable player
+		String message = "Error, try entering again";//creates variable message and gives it a value of 0
+		if (player.equalsIgnoreCase("rock") && comp==1)//says if the value of player is equal to rock (capitalization doesn't matter) and the value of comp is 1, follow the instructions inside the bracket
 		{
 			message = "Computer: rock \r\nIt's a Tie";
 		}
-		else if (player.equalsIgnoreCase("paper") && comp==2)
+		else if (player.equalsIgnoreCase("paper") && comp==2)//says if the numbers haven't fit any of the previous criteria AND the value of player is equal to paper (capitalization doesn't matter) and the value of comp is 2, follow the instructions inside the bracket
 		{
 			message = "Computer: paper \r\nIt's a Tie";
 		}
-		else if (player.equalsIgnoreCase("scissors") && comp==3)
+		else if (player.equalsIgnoreCase("scissors") && comp==3)//says if the numbers haven't fit any of the previous criteria AND the value of player is equal to scissors (capitalization doesn't matter) and the value of comp is 3, follow the instructions inside the bracket
 		{
 			message = "Computer: scissors \r\nIt's a Tie";
 		}
-		else if (player.equalsIgnoreCase("rock") && comp==2)
+		else if (player.equalsIgnoreCase("rock") && comp==2)//says if the numbers haven't fit any of the previous criteria AND the value of player is equal to rock (capitalization doesn't matter) and the value of comp is 2, follow the instructions inside the bracket
 		{
 			message = "Computer: paper \r\nYou lose!";
 		}
-		else if (player.equalsIgnoreCase("paper") && comp==3)
+		else if (player.equalsIgnoreCase("paper") && comp==3)//says if the numbers haven't fit any of the previous criteria AND the value of player is equal to paper (capitalization doesn't matter) and the value of comp is 3, follow the instructions inside the bracket
 		{
 			message = "Computer: scissors \r\nYou lose!";
 		}
-		else if (player.equalsIgnoreCase("scissors") && comp==1)
+		else if (player.equalsIgnoreCase("scissors") && comp==1)//says if the numbers haven't fit any of the previous criteria AND the value of player is equal to scissors (capitalization doesn't matter) and the value of comp is 1, follow the instructions inside the bracket 
 		{
 			message = "Computer: rock \r\nYou lose!";
 		}
-		else if (player.equalsIgnoreCase("rock") && comp==3)
+		else if (player.equalsIgnoreCase("rock") && comp==3)//says if the numbers haven't fit any of the previous criteria AND the value of player is equal to rock (capitalization doesn't matter) and the value of comp is 3, follow the instructions inside the bracket
 		{
 			message = "Computer: scissors \r\nYou win!";
 		}
-		else if (player.equalsIgnoreCase("paper") && comp==1)
+		else if (player.equalsIgnoreCase("paper") && comp==1)//says if the numbers haven't fit any of the previous criteria AND the value of player is equal to paper (capitalization doesn't matter) and the value of comp is 1, follow the instructions inside the bracket 
 		{
 			message = "Computer: rock \r\nYou win!";
 		}
-		else if (player.equalsIgnoreCase("scissors") && comp==2)
+		else if (player.equalsIgnoreCase("scissors") && comp==2)//says if the numbers haven't fit any of the previous criteria AND the value of player is equal to scissors (capitalization doesn't matter) and the value of comp is 2, follow the instructions inside the bracket
 		{
 			message = "Computer: paper \r\nYou win!";
 			
