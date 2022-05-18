426b09828eec3fa9b7eba3340c4b95c4f0d9b03c
16261507 <16261507@L815WD220012.edu.cbe.ab.ca>
2022-05-18 12:49:46 -0600

    12:49 pm

--- Credit5/src/Credit/five/javaSwitch.java
+++ Credit5/src/Credit/five/javaSwitch.java
@@ -17,32 +17,30 @@ public class javaSwitch
 	public static void main(String[] args) 
 	{
 		int day = 2;//defines day as 2 
-		switch (day) 
+		switch (day) //opens new switch about variable day 
 		{
-			case 1: 
-				System.out.println("Saturday");
-				break;
+			case 1: //if user types one, follow instructions too break and beyond, if not, skip ahead to break 
+				System.out.println("Saturday");//display Saturday 
+				break;//end of case 
 		  
-			case 2:
-				System.out.println("Sunday");
-				break;
+			case 2://if user types two, follow instructions too break and beyond, if not, skip ahead to break 
+				System.out.println("Sunday");//display Sunday 
+				break;//end of case 
 		}
-		int day2 = 4;
-		switch (day2) 
+		int day2 = 4;//defines variable day2 as 4 
+		switch (day2) //opens new switch about variable day2 
 		{
-			case 1:
-				System.out.println("Saturday");
-				break;
-			case 2:
-		    System.out.println("Sunday"); 
-		    break;
+			case 1://if user types one, follow instructions too break and beyond, if not, skip ahead to break 
+				System.out.println("Saturday");//display Saturday
+				break;//end of case
+			case 2://if user types two, follow instructions too break and beyond, if not, skip ahead to break 
+		    System.out.println("Sunday"); //display Sunday 
+		    break;//end of case
 		  
-		default:
-		    System.out.println("Weekend");
+		default://if not one or two, follow the following instructions 
+		    System.out.println("Weekend");//Display Weekend 
 		}
-
 	}
-
 }
 /* Screen Dump
 Sunday
